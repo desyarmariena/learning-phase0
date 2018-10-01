3e6a11f1063739a4e1f79a48d24845e8aeebdd50
Desy Armariena <desy.armariena@gmail.com>
2018-10-01 23:10:36 +0700

    Array - Find Mean Number

---
+++ w3-exercise9.js
@@ -0,0 +1,31 @@
+/* 
+Exercise 9 - Mencari Mean
+*/
+
+/* Diberikan sebuah function cariMean(arr) 
+yang menerima sebuah array angka. 
+Function akan me-return mean dari array 
+atau deret angka tersebut. 
+Mean adalah angka rata-rata dari 
+deret bilangan tersebut. 
+Contoh, mean atau rata-rata dari 
+[1, 2, 3, 4, 5] adalah 3. Kita perlu 
+kemudian melakukan pembulatan angka dari 
+hasil mean yang didapatkan. */
+
+
+
+function cariMean(arr) {
+    var total = 0;
+    for(var i = 0; i < arr.length; i++){
+        total += arr[i];
+    }
+    return Math.round(total/arr.length);
+}
+
+// TEST CASES
+console.log(cariMean([1, 2, 3, 4, 5])); // 3
+console.log(cariMean([3, 5, 7, 5, 3])); // 5
+console.log(cariMean([6, 5, 4, 7, 3])); // 5
+console.log(cariMean([1, 3, 3])); // 2
+console.log(cariMean([7, 7, 7, 7, 7])); // 7
