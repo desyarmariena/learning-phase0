b3746d22ca0aee4f5ff87ed6f43e1fad69557ced
Desy Armariena <desy.armariena@gmail.com>
2018-10-01 22:53:33 +0700

    Array - Polindrome

---
+++ w3-exercise5.js
@@ -0,0 +1,25 @@
+/* 
+Exercise 5 - Palindrome
+*/
+
+/* Diberikan sebuah function palindrome(kata) 
+yang menerima satu parameter. 
+Function tersebut akan mengembalikan nilai true 
+jika kata merupakan palindrome, dan false jika bukan. 
+Kata palindrome adalah sebuah kata yang jika dibalik, 
+tetap sama. Contoh, 'katak' dibalik tetaplah 'katak'. */
+
+function palindrome(kata) {
+    var kataReversed = '';
+    for(var i = kata.length-1; i >= 0; i--){
+        kataReversed += kata[i];
+    }
+    return kata === kataReversed;
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
