1327e006b44a063d4d8d4a21db8bbaae9faa01d2
Desy Armariena <desy.armariena@gmail.com>
2018-10-01 10:02:51 +0700

    Refactor switch case exercise

--- w2-exercise4.js
+++ w2-exercise4.js
@@ -6,26 +6,34 @@ Misal tanggal yang diberikan adalah hari 1, bulan 5, dan tahun 1945.
 Maka, output yang harus kamu proses adalah menjadi 1 Mei 1945. 
 */
 
-var hari, bulan, tahun;
+var hari = 1;
+var bulan = 10;
+var tahun = 2018;
 
-hari = prompt('Input tanggal! (1 - 31)');
-bulan = prompt('Input bulan! (1 - 12)');
-tahun = prompt('Input tahun! (1900 - 2200)');
-
-// dengan asumsi user pasti input dengan benar
-switch(bulan){
-  case '1': bulan = 'Januari'; break;
-  case '2': bulan = 'Februari'; break;
-  case '3': bulan = 'Maret'; break;
-  case '4': bulan = 'April'; break;
-  case '5': bulan = 'Mei'; break;
-  case '6': bulan = 'Juni'; break;
-  case '7': bulan = 'Juli'; break;
-  case '8': bulan = 'Agustus'; break;
-  case '9': bulan = 'September'; break;
-  case '10': bulan = 'Oktober'; break;
-  case '11': bulan = 'November'; break;
-  case '12': bulan = 'Desember'; break;
+if(hari >= 1 && hari <= 31){
+  if(bulan >= 1 && bulan <= 12){
+    if(tahun >= 1900 && tahun <= 2200){
+      switch(bulan){
+        case 1: bulan = 'Januari'; break;
+        case 2: bulan = 'Februari'; break;
+        case 3: bulan = 'Maret'; break;
+        case 4: bulan = 'April'; break;
+        case 5: bulan = 'Mei'; break;
+        case 6: bulan = 'Juni'; break;
+        case 7: bulan = 'Juli'; break;
+        case 8: bulan = 'Agustus'; break;
+        case 9: bulan = 'September'; break;
+        case 10: bulan = 'Oktober'; break;
+        case 11: bulan = 'November'; break;
+        case 12: bulan = 'Desember'; break;
+      }
+      console.log(`Tanggal yang dimasukkan`, hari, bulan, tahun);
+    } else {
+      console.log('Anda salah memasukkan tahun! (1900 - 2200');
+    }  
+  } else {
+    console.log('Anda salah memasukkan bulan! (1 - 12');
+  }
+} else {
+  console.log('Anda salah memasukkan tanggal! (1 - 31)');
 }
-
-console.log(`Tanggal yang dimasukkan `, hari, bulan, tahun);
