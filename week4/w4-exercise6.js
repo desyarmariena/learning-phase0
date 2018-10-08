c7ac91eb45fcc33494b5a35d2558eac12edb041a
Desy Armariena <desy.armariena@gmail.com>
2018-10-08 20:44:51 +0700

    Move to each folder

---
+++ week4/w4-exercise6.js
@@ -0,0 +1,4 @@
+// Exercise 6 - Hitung Huruf
+
+/* Diberikan function hitungHuruf(kalimat) yang akan menerima satu parameter berupa string. Function akan me-return kata dari kalimat yang memiliki jumlah perulangan huruf yang paling besar. */
+
