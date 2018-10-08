1e776b6bce48fce070e9bf7d253dbc1b5ef261ba
Desy Armariena <desy.armariena@gmail.com>
2018-10-08 21:44:37 +0700

    Week 4 - Find Modus

--- week4/w4-exercise4.js
+++ week4/w4-exercise4.js
@@ -4,6 +4,42 @@
 
 function cariModus(arr) {
     // you can only write your code here!
+    var banyakAngkaMuncul = [];
+    
+    //hitung jumlah perulangan kata
+    for(var i = 0; i < arr.length; i++){
+        var temp = 0;
+        for(var j = 0; j < arr.length; j++){
+            if(arr[i] === arr[j]){
+                temp++;
+            }
+        }
+        banyakAngkaMuncul.push(temp);
+    }
+
+    //cari modus
+    //cek apakah jumlah angka muncul dalam array sama / tidak ada modus
+    var hasModus = false;
+    for(var i = 0; i < banyakAngkaMuncul.length-1; i++){
+        if(banyakAngkaMuncul[i] !== banyakAngkaMuncul[i+1]){
+            hasModus = true; break;
+        }
+    }
+    if(hasModus){
+        var indeks = 0;
+        var frekuensiTerbanyak = banyakAngkaMuncul[0];
+        //cari frekuensi terbanyak muncul
+        for(var i = 1; i < banyakAngkaMuncul.length-1; i++){
+            if(banyakAngkaMuncul[i] > banyakAngkaMuncul[i+1] && 
+                frekuensiTerbanyak !== banyakAngkaMuncul[i]){
+                indeks = i;
+                frekuensiTerbanyak = banyakAngkaMuncul[i];
+            }
+        }        
+        return arr[indeks];
+    } else {
+        return -1;
+    }
 }
 
 // TEST CASES
