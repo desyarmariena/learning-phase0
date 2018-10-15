9b86b513b09b1907e13311499b326160c5dcf9b1
Desy Armariena <desy.armariena@gmail.com>
2018-10-15 23:12:37 +0700

    Week 5 - Deep Sum

---
+++ week5/w5-exercise1.js
@@ -0,0 +1,64 @@
+// Exercise 1 - Deep Sum
+
+// Implementasikan function deepSum untuk mendapatkan jumlah pertambahan dari angka-angka yang terdapat di dalam array
+
+function deepSum (arr) {
+    // Code disini
+    var sum = 0;
+
+    if (arr.length === 0){
+        return 'No number';
+    }
+
+    for(var i = 0; i < arr.length; i++){
+        for(var j = 0; j < arr[i].length; j++){
+            for(var k = 0; k < arr[i][j].length; k++){
+                sum += arr[i][j][k];
+            }
+        }
+    }
+
+    return sum;
+  }
+  
+  //TEST CASE
+  console.log(deepSum([
+    [
+      [4, 5, 6],
+      [9, 1, 2, 10],
+      [9, 4, 3]
+    ],
+    [
+      [4, 14, 31],
+      [9, 10, 18, 12, 20],
+      [1, 4, 90]
+    ],
+    [
+      [2, 5, 10],
+      [3, 4, 5],
+      [2, 4, 5, 10]
+    ]
+  ])); // 316
+  
+  console.log(deepSum([
+    [
+      [20, 10],
+      [15],
+      [1, 1]
+    ],
+    [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      [2],
+      [9, 11]
+    ],
+    [
+      [3, 5, 1],
+      [1, 5, 3],
+      [1]
+    ],
+    [
+      [2]
+    ]
+  ])); // 156
+  
+  console.log(deepSum([])); // No number
