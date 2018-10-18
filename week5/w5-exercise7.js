744e3be223ecb24aa8ddf50e26c871cea3fb790f
Desy Armariena <desy.armariena@gmail.com>
2018-10-18 16:52:46 +0700

    Week 5 - Delete Symbols

---
+++ week5/w5-exercise7.js
@@ -0,0 +1,14 @@
+// Exercise 7 - Hapus Simbol
+
+function hapusSimbol(str) {
+    // you can only write your code here!   
+     
+    return str.match(/[a-z,0-9]/g).join('');
+}
+
+// TEST CASES
+console.log(hapusSimbol('test%$4aa')); // test4aa
+console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
+console.log(hapusSimbol('ma@#k!an~')); // makan
+console.log(hapusSimbol('coding')); // coding
+console.log(hapusSimbol('1+3-5*2=100')); // 1352100
